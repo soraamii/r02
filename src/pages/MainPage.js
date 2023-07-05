32e65698dc80ffe6e27f1b4b07b758457852fadd
soraamii <thfkazoa@gmail.com>
2023-07-05 17:21:00 +0900

    [0705] react Router

---
+++ src/pages/MainPage.js
@@ -0,0 +1,14 @@
+import BasicLayout from "../layouts/BasicLayout";
+
+
+const MainPage = () => {
+
+  console.log("Main");
+  return ( 
+    <BasicLayout>
+      Main Page
+    </BasicLayout>
+   );
+}
+ 
+export default MainPage;
---
+++ src/routers/root.js
@@ -0,0 +1,31 @@
+import {createBrowserRouter} from "react-router-dom";
+import MainPage from "../pages/MainPage";
+import AboutPage from "../pages/AboutPage";
+import ListPage from "../pages/board/ListPage";
+
+
+// 진짜 라우터
+const router = createBrowserRouter([
+  {
+    path:"",
+    element: <MainPage></MainPage>
+  }
+  ,
+  {
+    path:"about",
+    element: <AboutPage></AboutPage>
+  }
+  ,
+  {
+    path:"board",
+    //element: <div>Board</div>,
+    children: [
+      {
+      path: "list",
+      element: <ListPage></ListPage>
+      }
+    ]
+  }
+])
+
+export default router;
